6705db45923321c087fc2e83b9fe464fa0c9677b
dronus <paul.geisler@web.de>
2013-03-10 17:38:35 +0100

    vf_alphachromakey: replaced tabs by spaces

--- libavfilter/vf_alphachromakey.c
+++ libavfilter/vf_alphachromakey.c
@@ -50,8 +50,8 @@ typedef struct {
 } AlphaChromakeyContext;
 
 static const char *const var_names[] = {
-	"t",
-	NULL
+    "t",
+    NULL
 };
 
 #define FLAGS AV_OPT_FLAG_VIDEO_PARAM|AV_OPT_FLAG_FILTERING_PARAM
@@ -120,7 +120,7 @@ static int config_output(AVFilterLink *outlink)
 
 static int clamp (int x, int a, int b)
 {
-	return (x < a ? a :  (x>b ? b : x) );
+    return (x < a ? a :  (x>b ? b : x) );
 }
 
 static void draw_frame(AVFilterContext *ctx,
@@ -133,10 +133,10 @@ static void draw_frame(AVFilterContext *ctx,
     
     FILE* file=fopen("uv.txt","r");
     if(file){
-	char buffer[255];
-	fgets(buffer,255,file);
+    char buffer[255];
+    fgets(buffer,255,file);
         av_opt_set_from_string(keyer, buffer, shorthand, "=", ":");
-	fclose(file);
+    fclose(file);
     }
 
     double alpha=1;
@@ -144,13 +144,13 @@ static void draw_frame(AVFilterContext *ctx,
             AVFilterLink *inlink = ctx->inputs[0];
             
             double vars[]={TS2D(main_buf->pts) * av_q2d(inlink->time_base)};
-	    int ret=av_expr_parse_and_eval(&alpha, keyer->alpha_expr,
-		                   var_names, vars,
-		                   NULL, NULL, NULL, NULL, NULL, 0, ctx);
-	    if(ret<0){
-		av_log(ctx, AV_LOG_ERROR, "Bad alpha expression.\n");    	
-		return AVERROR(EINVAL);
-	    }
+        int ret=av_expr_parse_and_eval(&alpha, keyer->alpha_expr,
+                           var_names, vars,
+                           NULL, NULL, NULL, NULL, NULL, 0, ctx);
+        if(ret<0){
+        av_log(ctx, AV_LOG_ERROR, "Bad alpha expression.\n");        
+        return AVERROR(EINVAL);
+        }
     }
     int h = main_buf->video->h;
 // TODO
@@ -169,48 +169,48 @@ static void draw_frame(AVFilterContext *ctx,
             }
         }
     } else {
-    	long sum_u=0, sum_v=0, count=0;
+        long sum_u=0, sum_v=0, count=0;
         for (y = 0; y < h; y++) {
             int yfact = (main_buf->format==AV_PIX_FMT_YUVA422P ? y : y/2);
             uint8_t* in_u  = main_buf->data[U] + yfact  * main_buf->linesize[U];
             uint8_t* in_v  = main_buf->data[V] + yfact  * main_buf->linesize[V];
             uint8_t* pout  = main_buf->data[A] + y * main_buf->linesize[A];
             for (int x = 0; x < main_buf->linesize[A]; x++) {
-            	
-            	/*
-            	// variant 1: abs ramp with offset and scale
-            	int du=abs(in_u[x/2] - keyer->u)*keyer->uw/16;
-            	int dv=abs(in_v[x/2] - keyer->v)*keyer->vw/16;
-            	int r=du+dv-keyer->offset;
-            	pout[x] = (uint8_t) clamp(r,0,255);
-            	*/
-            	
-            	// variant 2: radius threshold with feather, double precision
-            	double du=in_u[x/2]/256.0 - keyer->u/256.0, dv=in_v[x/2]/256.0 - keyer->v/256.0;
-            	double r=sqrt(du*du+dv*dv);
-            	double tola=keyer->min/256.0;
-            	double tolb=keyer->max/256.0;
-            	if (r<tola)      r=0;
-            	else if (r<tolb) r=(r-tola)/(tolb-tola);
-            	else             r=1;
-            	r*=alpha;
-            	pout[x]=(int)(r*255);
-            	
-            	// variant 3: squared radius threshold with feather, integer
-     /*       	int du=in_u[x/2] - (int)keyer->u, dv=in_v[x/2] - (int)keyer->v;
-            	int r2=sqrt(du*du+dv*dv);
-             	int tola2=keyer->uw;
-            	int tolb2=keyer->vw;
+                
+                /*
+                // variant 1: abs ramp with offset and scale
+                int du=abs(in_u[x/2] - keyer->u)*keyer->uw/16;
+                int dv=abs(in_v[x/2] - keyer->v)*keyer->vw/16;
+                int r=du+dv-keyer->offset;
+                pout[x] = (uint8_t) clamp(r,0,255);
+                */
+                
+                // variant 2: radius threshold with feather, double precision
+                double du=in_u[x/2]/256.0 - keyer->u/256.0, dv=in_v[x/2]/256.0 - keyer->v/256.0;
+                double r=sqrt(du*du+dv*dv);
+                double tola=keyer->min/256.0;
+                double tolb=keyer->max/256.0;
+                if (r<tola)      r=0;
+                else if (r<tolb) r=(r-tola)/(tolb-tola);
+                else             r=1;
+                r*=alpha;
+                pout[x]=(int)(r*255);
+                
+                // variant 3: squared radius threshold with feather, integer
+     /*           int du=in_u[x/2] - (int)keyer->u, dv=in_v[x/2] - (int)keyer->v;
+                int r2=sqrt(du*du+dv*dv);
+                 int tola2=keyer->uw;
+                int tolb2=keyer->vw;
  
-            	if      (r2<tola2) r2=0;
-            	else if (r2<tolb2) r2=(r2-tola2)*256 / (tolb2-tola2);
-            	else               r2=255;
-            	pout[x]=r2;            	*/
-            	
-            	sum_u+=in_u[x/2]; 
-            	sum_v+=in_v[x/2];
-           	count++;            	
-            	
+                if      (r2<tola2) r2=0;
+                else if (r2<tolb2) r2=(r2-tola2)*256 / (tolb2-tola2);
+                else               r2=255;
+                pout[x]=r2;                */
+                
+                sum_u+=in_u[x/2]; 
+                sum_v+=in_v[x/2];
+                count++;                
+                
                 /* if(x % 4 ==0){
                     in_u += 1;
                     in_v += 1;
